265197d28f9a2736d8fbfd7fafdeb8b1382b46e5
Apurba Panja <apurbapanja1@gmail.com>
2025-06-17 20:56:10 +0530

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    clean: true,
+    library: {
+        name: 'EchoUIKit',
+        type: 'umd',
+    },
+    externals: {
+        react: 'react',
+        'react-dom': 'react-dom',
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$',
+                exclude: '/node_modules/',
+                use: 'babel-loader',
+            },
+            {
+                test: '/\.css$',
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            }
+        ]
+    }
+}
