5a1199942a2e4178e166836513297111e0db7d92
Apurba Panja <apurbapanja1@gmail.com>
2025-06-18 22:37:15 +0530

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports =  {
   mode: "production",
+  devtool: 'inline-source-map',
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
